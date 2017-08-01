7dfe61aed285b4afd5b2b915947fa2473cc451f1
chosenman <chosenman2008@gmail.com>
2017-08-01 15:07:07 -0700

    started SLL java

---
+++ OOP/SLL/ExeSll.java
@@ -0,0 +1,6 @@
+class ExeSll {
+  public static void main(String[] arg) {
+
+      Node firstNode = new Node("1");
+  }
+}
---
+++ OOP/SLL/Node.java
@@ -0,0 +1,9 @@
+class Node {
+  private String value;
+  private Object next;
+
+  Node(String val){
+    this.value = val;
+    this.next = null;
+  }
+}
