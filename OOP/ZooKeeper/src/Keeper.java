69de373c4ede5471264cde7c4c904d04ada3cfe0
chosenman <chosenman2008@gmail.com>
2017-08-02 10:57:40 -0700

    started zoo keeper 1

---
+++ OOP/ZooKeeper/src/Keeper.java
@@ -0,0 +1,8 @@
+
+public class Keeper {
+	public static void main(String[] arg) {
+		Mammal testMammal = new Mammal();
+		
+		testMammal.displayEnergy();
+	}
+}
---
+++ OOP/ZooKeeper/src/Mammal.java
@@ -0,0 +1,12 @@
+
+public class Mammal {
+	int energyLevel;
+	
+	public Mammal() {
+		this.energyLevel = 0;
+	}
+	
+	void displayEnergy() {
+		System.out.println(this.energyLevel);
+	}
+}
