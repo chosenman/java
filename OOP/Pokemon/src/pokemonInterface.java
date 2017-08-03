a6f8895a8a6aeee9636b66ccac49028d21781873
chosenman <chosenman2008@gmail.com>
2017-08-02 19:06:05 -0700

    finished Pokemon assignment, Abstract classes, interfaces, methods in interface: default, static, abstract

--- OOP/Pokemon/src/pokemonInterface.java
+++ OOP/Pokemon/src/pokemonInterface.java
@@ -1,10 +1,14 @@
 
 public interface pokemonInterface {
+	// New in Java 8, default methods allow developers to create default methods that do not need implementation.
 	
+	// Abstract methods are methods that are declared without implementation. We saw an example of that in the OperateBicycle example above. 
+	// Abstract is the implicit method type; without declaration of either Static or default the method will be Abstract
 	 void createPokemon(String name, String type, int health);
 	 
 	 void attackPokemon(Pokemon pokemon);
 	 
+	 // Static methods are interface member methods. Also new to Java 8, they are implemented on the interface level and are called on the interface, not the class. Therefore, you cannot override static interface methods. You can only call them directly on the interface that they were declared.
 	 static String pokemonInfo(Pokemon pokemon) {
 		 return "Pokemon's name: " + pokemon.name + ", Health: " 
 				 + Integer.toString(pokemon.health) + 
