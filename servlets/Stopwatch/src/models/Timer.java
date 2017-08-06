561ff2bf16d3349a0bf38d16b40bc8d7dee8f186
chosenman <chosenman2008@gmail.com>
2017-08-05 23:57:35 -0700

    finished Time class wich operating with time

--- servlets/Stopwatch/src/models/Timer.java
+++ servlets/Stopwatch/src/models/Timer.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.text.Format;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -10,12 +11,46 @@ public class Timer {
 	String startTime;
 	String stopTime;
 	
+	public Timer() {
+		
+	}
+	
+	// start time
+	public void setStartTime() {
+		this.startTime = generateTimeNow();
+	}
+	public String getstartTime() {
+		return this.startTime;
+	}
+	
+	// stop time
+	public void setStopTime() {
+		this.stopTime = generateTimeNow();
+	}
+	public String getStopTime() {
+		return this.stopTime;
+	}
+	
+	// generate time NOW in String
 	public String generateTimeNow() {
-		Date now = Calendar.getInstance().getTime(); 
-		Format formatter = new SimpleDateFormat("HH:mm:ss a");
-		String nowString = formatter.format(now);
-//		formatter.get
-		return nowString;
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss a");
+		Date now = new Date();
+
+		String dateAsString = sdf.format(now); //"23:27:43 PM"
+		return dateAsString;
+	}
+	// parse string time to Date format
+	public Date dateFromString(String stringDate) {
+		SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss a");
+		Date dateFromString = null;
+		try {
+			dateFromString = sdf.parse(stringDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return dateFromString;
 	}
 
 	
